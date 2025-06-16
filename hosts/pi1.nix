70e8b520c6b0eabd48f59084897fe51841a89331
rawalcher <rawalcher@edu.aau.at>
2025-06-16 22:47:35 +0200

    make pi ready for first run

--- hosts/pi1.nix
+++ hosts/pi1.nix
@@ -70,7 +70,30 @@
   services.openssh.enable = true;
 
   environment.systemPackages = with pkgs; [
-    git vim wget curl age
+    git vim wget curl age rustc cargo
+
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    python3Packages.wheel
+
+    gcc
+    glibc.dev
+    pkg-config
+
+    zlib.dev
+    libffi.dev
+    openssl.dev
+
+    cmake
+
+    python3Packages.numpy
+    python3Packages.pillow
+  ];
+
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+  boot.kernelParams = [
+    "gpu_mem=256"
   ];
 
   nix = {
@@ -78,6 +101,43 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+
+  # setup script for the venv
+  environment.etc."setup-traffic-venv.sh" = {
+    text = ''
+      #!/bin/bash
+      cd /home/nixos/rust-traffic-watch
+
+      if [ ! -d ".venv" ]; then
+        echo "Creating virtual environment..."
+        python3 -m venv .venv
+        source .venv/bin/activate
+
+        pip install --upgrade pip wheel
+
+        pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+        pip install ultralytics
+        pip install opencv-python-headless
+        pip install pillow numpy pandas matplotlib seaborn
+        pip install requests pyyaml tqdm psutil
+
+        echo "Virtual environment setup complete!"
+      else
+        echo "Virtual environment already exists"
+      fi
+    '';
+    mode = "0755";
   };
 
   system.stateVersion = "23.11";
