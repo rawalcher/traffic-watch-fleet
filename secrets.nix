cbe037b2488ce17f2495795f1b07cf33544935d5
rawalcher <rawalcher@edu.aau.at>
2025-06-17 22:39:53 +0200

    added nuc public key and --rekey

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,7 @@
 let
   arch_pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/2r/UUYwIhL/LCNzNNCuK0U7eWSreXe2P7j4WXTFsQ rawalcher@desktop";
 
-  nuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@traffic-nuc";
+  nuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/4fJWyfiu4la4EW36sBgQICgWxZ0PEPTQSy0TRwntv root@traffic-nuc";
   pi4_1gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@traffic-pi4-1gb";
   pi4_2gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@traffic-pi4-2gb";
   pi4_8gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@traffic-pi4-8gb";
@@ -12,5 +12,4 @@ in
 {
   "secrets/deploy-key.age".publicKeys = users ++ systems;
   "secrets/deploy-key-traffic.age".publicKeys = users ++ systems;
-  "secrets/wifi-password.age".publicKeys = users ++ systems;
 }
