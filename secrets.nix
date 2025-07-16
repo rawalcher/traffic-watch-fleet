630d0ad2f5bd429a64e210ad6ae518032f5ab979
rawalcher <rawalcher@edu.aau.at>
2025-07-16 20:45:50 +0200

    add pi4-8gb to secrets

--- secrets.nix
+++ secrets.nix
@@ -4,7 +4,7 @@ let
   nuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/4fJWyfiu4la4EW36sBgQICgWxZ0PEPTQSy0TRwntv root@nixos";
   pi4_1gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@nixos";
   pi4_2gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@traffic-pi4-2gb";
-  pi4_8gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBM7qyt6V6vklLNMsRmMohCK+0uSCqe6qqg1RYIYNSkl root@traffic-pi4-8gb";
+  pi4_8gb = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVmfxwDVZVK2Ejhd9qWy+QSyJI6jP7s5kEqhs4+EO74 root@nixos";
 
   users = [ arch_pc ];
   systems = [ nuc pi4_1gb pi4_2gb pi4_8gb ];
